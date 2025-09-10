72ab4188f9b2bea8ad8c1f8ab054d72df485cae5
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-09-10 17:05:13 +0600

    backend vue separaate folder created

---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+const mix = require('laravel-mix');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+
+mix.setResourceRoot('resources')
+    .setPublicPath('public')
+    .sourceMaps(true, 'source-map')
+    .sass('resources/scss/backend.scss', 'public/css/backend.css')
+    .copy('resources/js/crud.js', 'public/js/crud.js')
+    // .styles([
+    //     'resources/css/autocomplete.css',
+    //     'resources/css/sayt.css',
+    //     'resources/css/jquery-ui.css',
+    // ], 'public/css/sayt.css')
+    // .copyDirectory('resources/js', 'public/js')
+    .version();

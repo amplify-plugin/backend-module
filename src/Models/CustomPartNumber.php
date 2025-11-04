9d2f10d5f95c1a2201fc02283a7373f96e544bce
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-11-04 06:06:09 +0600

    custom part number crud added

--- src/Models/CustomPartNumber.php
+++ src/Models/CustomPartNumber.php
@@ -3,13 +3,11 @@
 namespace Amplify\System\Backend\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CustomPartNumber extends Model
 {
     use CrudTrait;
-    use HasFactory;
 
     /*
     |--------------------------------------------------------------------------

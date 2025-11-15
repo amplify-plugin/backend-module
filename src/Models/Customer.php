d1a5f5a6bf38c321d1691c10cf0c9baea2b6f073
hafijul1996 <hafijul1996@users.noreply.github.com>
2025-11-15 18:40:15 +0000

    ACP-135 other: fixing php code style issues

--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -55,9 +55,9 @@ class Customer extends Model implements Auditable
 
         static::deleting(function ($model) {
 
-            $model->contacts->each(fn(Contact $contact) => $contact->delete());
+            $model->contacts->each(fn (Contact $contact) => $contact->delete());
 
-            $model->addresses->each(fn(CustomerAddress $customerAddress) => $customerAddress->delete());
+            $model->addresses->each(fn (CustomerAddress $customerAddress) => $customerAddress->delete());
         });
 
     }
@@ -154,7 +154,7 @@ class Customer extends Model implements Auditable
      */
     public function setPunchOutConfigurationAttribute($value)
     {
-        $this->attributes['punch_out_configuration'] = (bool)$this->punch_out
+        $this->attributes['punch_out_configuration'] = (bool) $this->punch_out
             ? $value
             : null;
     }
@@ -162,6 +162,6 @@ class Customer extends Model implements Auditable
     // Define the display_name accessor
     public function getDisplayNameAttribute()
     {
-        return $this->customer_name . ' - ' . $this->customer_code;
+        return $this->customer_name.' - '.$this->customer_code;
     }
 }

11b91c33a73d22f16ca2d4f3c43c5a835c1a6757
arifeasyask <ariful.islam@easyask.com>
2025-11-23 15:55:48 +0600

    :pencil2: Add: => quotation functionality

--- src/Models/CustomerOrder.php
+++ src/Models/CustomerOrder.php
@@ -306,6 +306,42 @@ class CustomerOrder extends Model implements Auditable
                         'success' => true,
                         'message' => 'Quotation Received',
                     ];
+                } elseif ($data['order_type'] == 'Q') {
+                    $orderResponse = ErpApi::createOrder([
+                        'order' => $order_infos,
+                        'items' => $products->toArray(),
+                    ]);
+
+                    if (isset($orderResponse->OrderStatus) && $orderResponse->OrderStatus === 'Accepted') {
+
+                        $this->update([
+                            'erp_order_id' => $orderResponse?->OrderNumber ?? null,
+                            'order_status' => 'Quotation',
+                        ]);
+
+                        NotificationFactory::call([Event::QUOTATION_RECEIVED], [
+                            'order_id' => $this->id,
+                            'customer_id' => $this->customer_id,
+                            'guest_customer_email' => ! customer_check() ? $order_infos['customer_email'] : null,
+                            'guest_customer_name' => ! customer_check() ? $order_infos['customer_name'] : null,
+                            'contact_id' => $this->contact_id ?? null,
+                        ]);
+
+                        return [
+                            'success' => true,
+                            'message' => 'Quotation Received',
+                            'order_id' => $orderResponse->OrderNumber,
+                        ];
+                    }
+
+                    $this->update([
+                        'order_status' => 'Rejected',
+                    ]);
+
+                    return [
+                        'success' => false,
+                        'message' => 'Quotation Rejected',
+                    ];
                 }
             }
 

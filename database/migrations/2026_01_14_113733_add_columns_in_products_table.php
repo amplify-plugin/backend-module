af656de883f38af05b5db86740046cdb463e27ed
Hafijul Islam <hafijul.islam@easyask.com>
2026-01-18 18:26:16 +0600

    Update 2026_01_14_113733_add_columns_in_products_table.php

--- database/migrations/2026_01_14_113733_add_columns_in_products_table.php
+++ database/migrations/2026_01_14_113733_add_columns_in_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->text('og_description')->nullable()->after('description');
+            $table->text('og_description')->nullable()->after('meta_description');
         });
     }
 

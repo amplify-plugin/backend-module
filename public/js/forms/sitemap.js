ba06759d3ffdbe56b634c67fbf388fccef5039b6
hafijul1996 <hafijul1996@users.noreply.github.com>
2026-02-21 00:07:42 +0000

    compiled laravel mix assets

--- public/js/forms/sitemap.js
+++ public/js/forms/sitemap.js
@@ -1,9 +1,26 @@
 crud.field("sitemapTags").subfield('type').onChange(function (field) {
-    crud.field("sitemapTags").subfield('location', field.rowNumber).show(field.value == 'image');
-    crud.field("sitemapTags").subfield('title', field.rowNumber).show(field.value == 'video');
-    crud.field("sitemapTags").subfield('description', field.rowNumber).show(field.value == 'video');
     crud.field("sitemapTags").subfield('thumbnail_loc', field.rowNumber).show(field.value == 'video');
     crud.field("sitemapTags").subfield('content_loc', field.rowNumber).show(field.value == 'video');
     crud.field("sitemapTags").subfield('player_loc', field.rowNumber).show(field.value == 'video');
     crud.field("sitemapTags").subfield('location', field.rowNumber).show(field.value == 'video');
 }).change();
+
+function formatFileNameFromUrl(url) {
+    try {
+        const cleanUrl = url.split('?')[0].split('#')[0];
+        const fileName = cleanUrl.substring(cleanUrl.lastIndexOf('/') + 1);
+        const nameWithoutExt = fileName.replace(/\.[^/.]+$/, '');
+
+        return nameWithoutExt
+            .replace(/[^a-zA-Z0-9-]+/g, ' ')
+            .toLowerCase()
+            .split(' ')
+            .filter(Boolean)
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ');
+
+    }
+    catch (error) {
+        return '';
+    }
+}

378df26590962d6fb0e8ce8d63e3b6c773169089
Zubayer Hossain <zubayerh4@gmail.com>
2026-03-12 02:20:52 +0600

    Added ActualSellPrice while creating Order

--- src/Models/CustomerOrder.php
+++ src/Models/CustomerOrder.php
@@ -184,7 +184,7 @@ class CustomerOrder extends Model implements Auditable
                     $price = $orderLine->customer_price;
                 }
 
-                return [
+                $data = [
                     'ItemNumber' => $orderLine->product_code,
                     'WarehouseID' => $orderLine?->warehouse->code ?? $CustomerDetails->DefaultWarehouse,
                     // @TODO: warehouse set from customer default
@@ -192,10 +192,17 @@ class CustomerOrder extends Model implements Auditable
                     'OrderQty' => $orderLine->qty,
                     'SourceType' => $orderLine->source_type ?? null,
                     'Source' => $orderLine->source ?? null,
-                    'Price' => $price,
                     'ItemComment' => ! empty($additionalInfo) && ! empty($additionalInfo?->OrderSpec) ? $additionalInfo?->OrderSpec : '',
                     'UnitOfMeasure' => $orderLine->unit_code ?? null,
                 ];
+
+                if (config('amplify.client_code') === 'MW') {
+                    $data['ActualSellPrice'] = $price;
+                }else{
+                    $data['Price'] = $price;
+                }
+
+                return $data;
             });
 
             if (! empty($data['shipping_option']) && config('amplify.client_code') === 'RHS') {

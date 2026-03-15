8c7438fb1356bb3fcba47e3a8c9574f96676086b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-03-15 23:48:49 +0600

    :pencil2: ACP => Added list price level option validation added

--- src/Http/Requests/CustomerRequest.php
+++ src/Http/Requests/CustomerRequest.php
@@ -45,6 +45,7 @@ class CustomerRequest extends FormRequest
             'state' => 'nullable|string',
             'country_code' => 'nullable|string',
             'zip_code' => 'nullable|string',
+            'list_price' => 'nullable|string|in:'. implode(array_keys(Customer::LIST_PRICES)),
 
             // contact validate
             'contact' => 'required|array|min:1',

1bd0e3850ab3f629b99e9bd70e0d2bceb8c56514
Zubayer Hossain <zubayerh4@gmail.com>
2026-06-22 20:59:55 +0600

    feat(ckeditor): centralize toolbar, templates, and assets in backend package

---
+++ resources/views/partials/ckeditor-scripts-inner.blade.php
@@ -0,0 +1,13 @@
+@php
+    $ckeditorEditorUrl = asset(config('amplify.backend.ckeditor.editor_url', 'packages/ckeditor/ckeditor.js'));
+    $ckeditorTemplatesUrl = asset('vendor/backend/ckeditor/content-templates.js');
+    $ckeditorTemplateImagesBase = asset('vendor/backend/ckeditor/plugins/templates/templates/images') . '/';
+@endphp
+
+<script>
+    window.__CKEDITOR_LOCAL_URL = @json($ckeditorEditorUrl);
+    window.__CKEDITOR_TEMPLATES_URL = @json($ckeditorTemplatesUrl);
+    window.__CKEDITOR_TEMPLATE_IMAGES_BASE = @json($ckeditorTemplateImagesBase);
+</script>
+<script src="{{ asset('vendor/backend/ckeditor/ckeditor-config.js') }}"></script>
+<script src="{{ asset('vendor/backend/ckeditor/ckeditor-vue-patch.js') }}"></script>
--- resources/views/partials/ckeditor-scripts.blade.php
+++ resources/views/partials/ckeditor-scripts.blade.php
@@ -1,15 +1,3 @@
-@php
-    $ckeditorEditorUrl = asset(config('amplify.backend.ckeditor.editor_url', 'packages/ckeditor/ckeditor.js'));
-    $ckeditorTemplatesUrl = asset('vendor/backend/ckeditor/content-templates.js');
-    $ckeditorTemplateImagesBase = asset('vendor/backend/ckeditor/plugins/templates/templates/images') . '/';
-@endphp
-
 @push('before_scripts')
-    <script>
-        window.__CKEDITOR_LOCAL_URL = @json($ckeditorEditorUrl);
-        window.__CKEDITOR_TEMPLATES_URL = @json($ckeditorTemplatesUrl);
-        window.__CKEDITOR_TEMPLATE_IMAGES_BASE = @json($ckeditorTemplateImagesBase);
-    </script>
-    <script src="{{ asset('vendor/backend/ckeditor/ckeditor-config.js') }}"></script>
-    <script src="{{ asset('vendor/backend/ckeditor/ckeditor-vue-patch.js') }}"></script>
+    @include('backend::partials.ckeditor-scripts-inner')
 @endpush

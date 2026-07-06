5ed80b9bcddfbdf434dd79f9dc88551558c6ba3c
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-07-07 00:34:52 +0600

    namespace typo corrected

--- src/Http/Controllers/Admin/CustomerGroupCrudController.php
+++ src/Http/Controllers/Admin/CustomerGroupCrudController.php
@@ -14,7 +14,7 @@ use Amplify\System\Backend\Models\PricingRule;
 use Amplify\System\Backend\Models\User;
 use Amplify\System\Backend\Models\VolumeDiscount;
 use Amplify\System\Backend\Models\VolumeDiscountDetail;
-use App\Http\Controllers\Admin\Exception;
+use Exception;
 use Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
 use Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
 use Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
@@ -69,32 +69,37 @@ class CustomerGroupCrudController extends BackpackCustomCrudController
      */
     protected function setupListOperation()
     {
-        CRUD::column('id')->type('number')->thousands_sep('');
-        CRUD::addColumn([
-            'name' => 'group_code',
-            'label' => 'Group Code',
+        CRUD::addColumns([
+            [
+                'name' => 'id',
+                'label' => 'ID',
+                'type' => 'number',
+            ],
+            [
+                'name' => 'group_name',
+                'label' => 'Name',
+            ],
+            [
+                'name' => 'group_code',
+                'label' => 'Code',
+            ],
+            [
+                'name' => 'group_pricing_type',
+                'label' => 'Pricing',
+                'type' => 'select_from_array',
+                'options' => CustomerGroup::CUSTOMER_GROUP_PRICING_TYPE,
+                'searchLogic' => function ($query, $column, $searchTerm) {
+                    $query->orWhere('group_pricing_type', 'like', '%' . Str::slug($searchTerm) . '%');
+                },
+            ],
+            [
+                'name' => 'users',
+                'label' => 'Assigned Users',
+                'type' => 'relationship',
+                'entity' => 'users',
+                'attribute' => 'name',
+            ]
         ]);
-        CRUD::addColumn([
-            'name' => 'group_name',
-            'label' => 'Group Name',
-        ]);
-        CRUD::addColumn([
-            'name' => 'group_pricing_type',
-            'label' => 'Group Pricing Type',
-            'type' => 'custom_html',
-            'value' => function ($entry) {
-                return CustomerGroup::CUSTOMER_GROUP_PRICING_TYPE[$entry->group_pricing_type];
-            },
-            'searchLogic' => function ($query, $column, $searchTerm) {
-                $query->orWhere('group_pricing_type', 'like', '%'.Str::slug($searchTerm).'%');
-            },
-        ]);
-
-        /**
-         * Columns can be defined using the fluent syntax or array syntax:
-         * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
-         */
     }
 
     /**
@@ -130,15 +135,10 @@ class CustomerGroupCrudController extends BackpackCustomCrudController
         ]);
 
         CRUD::field('customers');
+        CRUD::field('users');
         CRUD::field('pricing_rules');
 
         $this->data['customer_group_pricing_type'] = CustomerGroup::CUSTOMER_GROUP_PRICING_TYPE;
-
-        /**
-         * Fields can be defined using the fluent syntax or array syntax:
-         * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
-         */
     }
 
     /**
@@ -187,8 +187,8 @@ class CustomerGroupCrudController extends BackpackCustomCrudController
      */
     public function store(CustomerGroupRequest $request)
     {
+        DB::beginTransaction();
         try {
-            DB::beginTransaction();
             $customerGroupResponse = $this->traitStore($request);
             $this->syncUsers($customerGroupResponse['data'], $request);
             if (count($request->customers) > 0) {
@@ -233,8 +233,8 @@ class CustomerGroupCrudController extends BackpackCustomCrudController
      */
     public function update(CustomerGroupRequest $request)
     {
+        DB::beginTransaction();
         try {
-            DB::beginTransaction();
             $customerGroupResponse = $this->traitUpdate($request);
             $this->syncUsers($customerGroupResponse['data'], $request);
             $cg_id = $customerGroupResponse['data']->id;

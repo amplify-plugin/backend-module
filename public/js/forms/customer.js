4040020cc166aa7a8579c0f205a8e8ff0ca218a7
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-09-16 18:38:47 +0600

    backend old build code removed

--- public/js/forms/customer.js
+++
@@ -1,3 +0,0 @@
-crud.field('punch_out').onChange(function(field) {
-    crud.field('punch_out_configuration').show(field.value == 1);
-}).change();
--- public/js/forms/form-builder.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    crud.field('formFields').subfield('type').onChange(function (field) {
-        let value = field.value;
-        let rowNumber = field.rowNumber;
-         crud.field('formFields').subfield('options', rowNumber).show((['rSelect', 'rCheckbox', 'rRadio']).includes(value));
-         crud.field('formFields').subfield('minimum', rowNumber).show((['rRange', 'rSelectRange', 'rSelectYear']).includes(value));
-         crud.field('formFields').subfield('maximum', rowNumber).show((['rRange', 'rSelectRange', 'rSelectYear']).includes(value));
-    }).change();
-});
